310485f8a9c7170f99a50dae9b29bd02e8b7bd16
AksA1210 <95520592+AksA1210@users.noreply.github.com>
2023-07-17 23:09:44 +0530

    Add files via upload

---
+++ Final/Gauss_elimination.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue May 23 22:30:25 2023
+
+@author: user
+"""
+
+
+import numpy as np
+import sys
+
+print("\n-------------GAUSS ELIMINATION--------------\n")
+n = int(input("Enter number of unknowns : "))
+
+
+# for storing augmented matrix
+a = np.zeros((n,n+1))
+
+# for storing solution vector
+x = np.zeros(n)
+
+# Reading augmented matrix coefficients
+print('\nEnter Augmented Matrix Coefficients :')
+for i in range(n):
+    for j in range(n+1):
+        a[i][j] = float(input( 'a['+str(i)+']['+ str(j)+']='))
+
+# Applying Gauss Elimination
+for i in range(n):
+    if a[i][i] == 0.0:
+        sys.exit('Divide by zero detected!')
+        
+    for j in range(i+1, n):
+        ratio = a[j][i]/a[i][i]
+        
+        for k in range(n+1):
+            a[j][k] = a[j][k] - ratio * a[i][k]
+
+# Back Substitution
+x[n-1] = a[n-1][n]/a[n-1][n-1]
+
+for i in range(n-2,-1,-1):
+    x[i] = a[i][n]
+    
+    for j in range(i+1,n):
+        x[i] = x[i] - a[i][j]*x[j]
+    
+    x[i] = x[i]/a[i][i]
+
+
+print('\nRequired solution is : \n')
+for i in range(n):
+    print('X%d = %0.2f' %(i,x[i]), end = '\t')
